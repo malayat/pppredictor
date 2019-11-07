290f698ea342bd8b01b4eeb91a1d2f99c36a579b
Alejandro Ayala Ati <alejandro.ayala@solmedia.ec>
2019-11-07 01:23:20 -0500

    Initial commit

---
+++ src/main/kotlin/ec/solmedia/pppredictor/App.kt
@@ -0,0 +1,12 @@
+package ec.solmedia.pppredictor
+
+class App {
+    val greeting: String
+        get() {
+            return "Hello world."
+        }
+}
+
+fun main() {
+    println(App().greeting)
+}
