cc66d71eb106dcb2a3cf7374d280ba33f6b221ea
Alejandro Ayala Ati <alejandro.ayala@solmedia.ec>
2019-11-07 22:33:57 -0500

    Added information entry functionality

--- src/main/kotlin/ec/solmedia/pppredictor/App.kt
+++ src/main/kotlin/ec/solmedia/pppredictor/App.kt
@@ -1,12 +1,24 @@
 package ec.solmedia.pppredictor
 
+import java.util.*
+
 class App {
-    val greeting: String
-        get() {
-            return "Hello world."
-        }
+
+    fun enterInformation() =
+            Scanner(System.`in`).use {
+                print("Enter a licence plate number (ABDC123): ")
+                val licencePlateNumber = it.next()
+
+                print("Enter a date (dd/MM/yyyy): ")
+                val date = it.next()
+
+                print("Enter a time (17:31): ")
+                val time = it.next()
+
+                Information(licencePlateNumber, date, time)
+            }
 }
 
 fun main() {
-    println(App().greeting)
+    App().enterInformation()
 }
---
+++ src/main/kotlin/ec/solmedia/pppredictor/Information.kt
@@ -0,0 +1,7 @@
+package ec.solmedia.pppredictor
+
+class Information(
+        val licencePlateNumber: String?,
+        val date: String?,
+        val time: String?
+)
